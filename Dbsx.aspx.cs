989fe5351e370ad703498b9c16e74ba3491cb494
heguiqin <guiqin_he@163.com>
2020-03-17 13:51:11 +0800

    Update Dbsx.aspx.cs

--- Dbsx.aspx.cs
+++ Dbsx.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Dbsx : System.Web.UI.Page
+public partial class Dbsx : PageMain
 {
     protected void Page_Load(object sender, EventArgs e)
     {
