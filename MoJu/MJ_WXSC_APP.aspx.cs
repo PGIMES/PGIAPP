72a80e068854496c27fcb295b56d0a1bc043045a
heguiqin <guiqin_he@163.com>
2020-03-19 09:05:54 +0800

    ok

---
+++ MoJu/MJ_WXSC_APP.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class MoJu_MJ_WXSC_APP : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        GetData();
+    }
+
+    public DataSet Query(string SQLString)
+    {
+        using (SqlConnection connection = new SqlConnection(ConfigurationManager.AppSettings["connstringAPI"]))
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                connection.Open();
+                SqlDataAdapter command = new SqlDataAdapter(SQLString, connection);
+                command.SelectCommand.CommandTimeout = 0;
+                command.Fill(ds, "ds");
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return ds;
+        }
+
+    }
+
+    private void GetData()
+    {
+        DataTable dt = new DataTable();
+        string sql = @"exec API.dbo.SheBei_ForMes_Detail_Ver7_APP";
+        dt = Query(sql).Tables[0];
+
+        if (dt == null || dt.Rows.Count <= 0)
+        {
+            lb_msg.Text = "No Data Found!";
+        }
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+
+    }
+    protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        this.GridView1.PageIndex = e.NewPageIndex;
+        GetData();
+    }
+    protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            e.Row.Attributes.Add("onmouseover", "javascript:currentcolor=this.style.backgroundColor;this.style.backgroundColor='#DFE7DF';");
+            e.Row.Attributes.Add("onmouseout", "javascript:this.style.backgroundColor=currentcolor;");
+        }
+    }
+}
