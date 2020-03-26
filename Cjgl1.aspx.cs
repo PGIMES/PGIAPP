83dfa9188d4c8128aba494d052acc5d33f4ac3b2
heguiqin <guiqin_he@163.com>
2020-03-26 10:34:58 +0800

    问问

--- Cjgl1.aspx.cs
+++ Cjgl1.aspx.cs
@@ -6,13 +6,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Cjgl1 : PageMain
+public partial class Cjgl1 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //WeiXin.delCookies("userid");
-        //WeiXin.delCookies("workcode");
-        //WeiXin.delCookies("usermodel");
-        //Response.Write("cookie:" + WeiXin.GetCookie("workcode"));
+        
     }
 }
---
+++ workshop.aspx.cs
@@ -0,0 +1,18 @@
+﻿using LitJson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class workshop : PageMain
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //WeiXin.delCookies("userid");
+        //WeiXin.delCookies("workcode");
+        //WeiXin.delCookies("usermodel");
+        //Response.Write("cookie:" + WeiXin.GetCookie("workcode"));
+    }
+}
