0a9e1a46fd7f8ab62b661c923cb4ddea24ed956f
heguiqin <guiqin_he@163.com>
2020-04-09 10:12:17 +0800

    OK

--- WorkOrder/bhgp_Apply_list.aspx.cs
+++ WorkOrder/bhgp_Apply_list.aspx.cs
@@ -28,6 +28,7 @@ public partial class WorkOrder_bhgp_Apply_list : System.Web.UI.Page
     private void GetData(string emp)
     {
         DataTable dt_wk = new DataTable();
+        DataTable dt_wk_2 = new DataTable();
         //DataTable dt_my_1 = new DataTable();
         //DataTable dt_my_2 = new DataTable();
         //DataTable dt_my_3 = new DataTable();
@@ -37,12 +38,16 @@ public partial class WorkOrder_bhgp_Apply_list : System.Web.UI.Page
         DataSet ds = SQLHelper.Query(sql);
 
         dt_wk = ds.Tables[0];
+        dt_wk_2 = ds.Tables[1];
         //dt_my_1 = ds.Tables[1];
         //dt_my_2 = ds.Tables[2];
         //dt_my_3 = ds.Tables[3];
 
         list_go.DataSource = dt_wk;
         list_go.DataBind();
+
+        list_go_2.DataSource = dt_wk_2;
+        list_go_2.DataBind();
 
         //list_go_my.DataSource = dt_my_1;
         //list_go_my.DataBind();
@@ -64,8 +69,30 @@ public partial class WorkOrder_bhgp_Apply_list : System.Web.UI.Page
             DataRowView item = (DataRowView)e.Item.DataItem;
 
             DataTable dt_wk = new DataTable();
-            string sql = @"exec [usp_app_bhgp_Apply_list_dv] '{0}','{1}'";
-            sql = string.Format(sql, _workshop, item["stepname"].ToString());
+            string sql = @"exec [usp_app_bhgp_Apply_list_dv] '{0}','{1}','{2}'";
+            sql = string.Format(sql, _workshop, item["stepname"].ToString(), item["stepid"].ToString());
+            dt_wk = SQLHelper.Query(sql).Tables[0];
+
+            detail.DataSource = dt_wk;
+            detail.DataBind();
+
+            Label1.Text = "(" + dt_wk.Rows.Count + ")";
+
+
+        }
+    }
+
+    protected void list_go_2_ItemDataBound(object sender, RepeaterItemEventArgs e)
+    {
+        if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+        {
+            Repeater detail = (Repeater)e.Item.FindControl("re_go_2");
+            Label Label1 = (Label)e.Item.FindControl("Label1");
+            DataRowView item = (DataRowView)e.Item.DataItem;
+
+            DataTable dt_wk = new DataTable();
+            string sql = @"exec [usp_app_bhgp_Apply_list_dv] '{0}','{1}','{2}'";
+            sql = string.Format(sql, _workshop, item["stepname"].ToString(), item["stepid"].ToString());
             dt_wk = SQLHelper.Query(sql).Tables[0];
 
             detail.DataSource = dt_wk;
