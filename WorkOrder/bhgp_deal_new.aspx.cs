bc94709bb721a8c2780056ce38e4c88ac5d5fa92
heguiqin <guiqin_he@163.com>
2020-04-09 16:31:00 +0800

    OK

--- WorkOrder/bhgp_deal_new.aspx.cs
+++ WorkOrder/bhgp_deal_new.aspx.cs
@@ -28,11 +28,11 @@ public partial class WorkOrder_bhgp_deal_new : System.Web.UI.Page
 
         if (!IsPostBack)
         {
-            //LoginUser lu = (LoginUser)WeiXin.GetJsonCookie();
-            //emp_code_name.Text = lu.WorkCode + lu.UserName;
-            //domain.Text = lu.Domain;
-            emp_code_name.Text = "02432何桂勤";
-            domain.Text = "200";
+            LoginUser lu = (LoginUser)WeiXin.GetJsonCookie();
+            emp_code_name.Text = lu.WorkCode + lu.UserName;
+            domain.Text = lu.Domain;
+            //emp_code_name.Text = "02432何桂勤";
+            //domain.Text = "200";
 
             workorder.Text = _workorder;
             init_data(_workorder);
